30b2f5bdad50faa5effca3a6019c8144ea303b52
ijul27 <zulkarnain311216>
2023-01-16 00:22:12 +0700

    create page 3

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class=" w-[85%] m-auto fixed z-[] top-20 left-[5.7rem] border-b-2	 border-navbar"></div>
 
     <div class=" w-full h-[100vh] home">
-        <div class=" w-[80%] m-auto pt-[16rem] home-description">
+        <div class=" w-[80%] m-auto pt-[14rem] home-description">
             <h1 class="text-5xl tracking-wide leading-[4.5rem] opacity-70">AN INDIE<br>
                 WEB DEVELOPMENT AND<br>
                 TECHNOLOGY STUDIO.</h1>
@@ -41,7 +41,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <!-- end navbar -->
 
     <div class=" w-full h-[100vh] container-how-we-work">
-        <div class=" w-[80%] m-auto pt-[16rem] home-description">
+        <div class=" w-[80%] m-auto pt-[14rem] home-description">
             <span class=" block w-48 h-1 ml-[6px] bg-orange-500 rounded"></span>
             <h1 class="text-5xl tracking-wide leading-[4.5rem] opacity-70">HOW WE UNDERSTAND<br>
                         YOUR NEEDS & HOW WECAN<br>
@@ -49,6 +49,39 @@ import { RouterLink, RouterView } from 'vue-router'
             <p class=" w-[50%] text-xs opacity-60">We bring in our expertise and create a strategy and plan after understanding your
             needs and goals. We take personal stock in the work which leads to amazing executions!</p>
         </div>
+    </div>
+
+    <div class="  w-full h-[100vh] container-how-we-work-listen">
+
+    </div>
+
+    <div class=" w-full h-[100vh] container-technology-stack">
+        <div class="  w-[80%] m-auto pt-[6rem] text-center  technology-stack-description">
+            <h1 class=" text-4xl tracking-wide leading-[4.5rem] opacity-70 ">ADAPTIVE TECHNOLOGY</h1>
+            <h1 class=" text-4xl tracking-wide leading-[4.5rem] opacity-70 ">STACK!</h1>
+            <span class=" block w-40 h-1 m-auto bg-orange-500 rounded"></span>
+        </div>
+        
+        <div class=" w-[70%] m-auto pt-11 flex justify-center gap-6 flex-wrap card-technology-stack">
+
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            <div class=" w-32 h-20 bg-white rounded-[10px] card">Laravel</div>
+            
+        </div>
+
     </div>
 
   <RouterView />
